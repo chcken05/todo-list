94d74b5b86f7752522e14a0307c783f3563c4bc1
Michael Severino <michaeladrian.severino@gmail.com>
2026-06-08 02:18:52 +0800

    updated index.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
-alert("test");
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = false;
+    this.id = Date.now();
+  }
+}
+
+let todos = [];
+
+const addTodo = (title, description, dueDate, priority) => {
+  const todo = new Todo(title, description, dueDate, priority);
+  todos.push(todo);
+  return todo;
+};
