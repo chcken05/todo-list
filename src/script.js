3a393764866d5c93f03c52dd78ce3885860f6439
Michael Severino <michaeladrian.severino@gmail.com>
2026-06-08 23:34:05 +0800

    testing

--- src/script.js
+++ src/script.js
@@ -1,18 +1,3 @@
-class Todo {
-  constructor(title, description, dueDate, priority) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.completed = false;
-    this.id = Date.now();
-  }
-}
+import { listController } from "./listController.js";
 
-let todos = [];
-
-const addTodo = (title, description, dueDate, priority) => {
-  const todo = new Todo(title, description, dueDate, priority);
-  todos.push(todo);
-  return todo;
-};
+listController.log();
