f9e43c1cb45cc31f5184e852191f97d25d664bcf
chcken <michaeladrian.severino@gmail.com>
2026-06-27 14:42:51 +0800

    add styling

--- src/script.js
+++ src/script.js
@@ -2,3 +2,4 @@ import {todoController} from "./todos/todoController.js";
 import {todoView} from "./todos/todoView.js";
 import {projectController} from "./projects/projectController.js";
 import {projectView} from "./projects/projectView.js";
+import "./styles.css";
